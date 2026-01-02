2fb8b09110e257ce2d3b940bee7a9c3b331706ec
adanemisgie-git <adanemisgie01@gmail.com>
2026-01-02 11:14:53 +0300

    first commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -22,7 +22,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+// const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 export type State = {
   errors?: {
     customerId?: string[];
